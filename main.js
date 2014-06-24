b5b0f94d582d11ada3ad0f715a6cdcabd021a5ec
ryandagg <ryan.dagg@gmail.com>
2014-06-24 15:01:58 -0600

    Final

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-// random notes: 123-456-789
-// [1-9a-z!@#$%^&*~`-<>:;|,.?
-// phone[3] != '-' || phone[7] != '-'
-
 var wtf = function() {
 	alert("Nice try! But read the directions.")
 }
 
-// what functions to write?
 // something to replace certain characters, see if those specific characters are in the 
 // correct place and check if the rest is NaN
 var format_test = function(str, remove) {
@@ -34,20 +29,9 @@ var check_special = function(str, special, locations) {
 // phone validation
 var phone_val = function() {
 	var phone = prompt("Gimme them digits! (with dashes)");
-	if(phone.length != 12) {
+	if(phone.length != 12 || format_test(phone, "-") || check_special(phone, "-", [3, 7])) {
 		wtf();
 		return phone_val();
-	}
-	else if (format_test(phone, "-")) {
-		wtf();
-		return phone_val();
-	}
-	else if (check_special(phone, "-", [3, 7]))  {
-		wtf();
-		return phone_val();
-	}
-	else {
-		alert("Thank you!")
 	}
 }
 
@@ -57,24 +41,13 @@ phone_val();
 // Birth date val
 var b_date = function() {
 	var bday = prompt("When were you born? (this format: (MM/DD/YY)");
-	if(bday.length != 8) {
-		wtf();
-		return b_date();
-	}
-	else if (format_test(bday, "/")) {
-		wtf();
-		return b_date();
-	}
-	else if (check_special(bday, '/', [2, 5])) {
+	if(bday.length != 8 || check_special(bday, '/', [2, 5]) || format_test(bday, "/")) {
 		wtf();
 		return b_date();
 	}
 	else if (String(bday.substring(0,2)) > 12 || String(bday.substring(3,5)) > 31) {
 		wtf();
 		return b_date();
-	}
-	else {
-		alert("Thank you!")
 	}
 }
 
@@ -83,20 +56,9 @@ b_date();
 //  Postal Code val
 var postal_val = function() {
 	var postal = prompt("What's your zip? (this format: (XXXXX OR XXXXX-XXXX)");
-	if(postal.length != 5 && postal.length != 10) {
+	if(postal.length != 5 && postal.length != 10 || format_test(postal, "-") || (check_special(postal, '-', [5]) && postal.length === 10)) {
 		wtf();
 		return postal_val();
-	}
-	else if (format_test(postal, "-")) {
-		wtf();
-		return postal_val();
-	}
-	else if (check_special(postal, '-', [5]) && postal.length === 10) {
-		wtf();
-		return postal_val();
-	}
-	else {
-		alert("Thank you!")
 	}
 }
 
@@ -105,16 +67,9 @@ postal_val();
 // State abbreviation
 var st_val = function() {
 	var state = prompt("What's your state? (this format: 2 letters, all CAPS");
-	if(state.length != 2) {
+	if(state.length != 2 || /[1-9\W_]/g.test(state)) {
 		wtf();
 		return st_val();
-	}
-	else if (/[1-9\W_]/g.test(state)) {
-		wtf();
-		return st_val();
-	}
-	else {
-		alert("Thank you!")
 	}
 }
 
@@ -127,9 +82,8 @@ var married_val = function() {
 		wtf();
 		return married_val();
 	}
-	else {
-		alert("Thank you!")
-	}
 }
 
 married_val();
+
+alert("Thank you!")
